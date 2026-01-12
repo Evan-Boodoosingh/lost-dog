25ae308f2088a43ed7e3271030913e442ed57059
Evan Boodoosingh <evan9496@gmail.com>
2026-01-12 03:03:08 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -12,7 +12,7 @@ export function ContactSection() {
     >
       <div className="text-center space-y-2 md:space-y-4 pt-10 md:pt-0">
         <h2 className="text-3xl md:text-4xl lg:text-5xl text-white drop-shadow-2xl">
-          Seen Them? Let's Connect! 🎯
+          Got My Dog? Let's Connect! 🎯
         </h2>
         <p className="text-base md:text-lg lg:text-xl text-white drop-shadow-lg">
           "People's lives don't end when they die. It ends when they lose
